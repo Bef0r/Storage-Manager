31c440a0149df27f819f6aefd59f413428e86dfe
Albrecht Márk <alb.mark94@gmail.com>
2020-05-15 18:03:10 +0200

    Created new enum classes

---
+++ src/main/java/com/storage/repositories/entities/enums/Languages.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.storage.repositories.entities.enums;
+
+import jdk.internal.net.http.hpack.Huffman;
+
+/**
+ *
+ * @author Dell
+ */
+public enum Languages {
+    EN,HU,DE
+}
---
+++ src/main/java/com/storage/repositories/entities/enums/Materials.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.storage.repositories.entities.enums;
+
+import sun.nio.cs.KOI8_R;
+
+/**
+ *
+ * @author Dell
+ */
+public enum Materials {
+    ST52,
+    KO_2,
+    KO_11,
+    KO_3,
+    H_13,
+    AlMgSi1,
+    AlMg3,
+    Fe_310_0
+}
