60a0f7e69181e27b732cf83bc3ec5dc0f40cb5bc
Albrecht Márk <alb.mark94@gmail.com>
2020-05-22 13:06:02 +0200

    Created manager implements

---
+++ src/main/java/managers/FloorManagerImp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managers;
+
+import managers.interfaces.FloorManager;
+
+/**
+ *
+ * @author Dell
+ */
+public class FloorManagerImp implements FloorManager{
+    
+}
