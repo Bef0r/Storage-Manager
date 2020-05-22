2e46f4385c253f55347a7d62fe1f124b5b3a9332
Albrecht Márk <alb.mark94@gmail.com>
2020-05-22 16:47:53 +0200

    Created roles response class

---
+++ src/main/java/com/storage/api/response/RoleAndPermission/RolesResponse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.storage.api.response.RoleAndPermission;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RolesResponse {
+    private Map<Long,String> Roles = new HashMap<>();
+
+    public Map<Long, String> getRoles() {
+        return Roles;
+    }
+
+    public void setRoles(Map<Long, String> Roles) {
+        this.Roles = Roles;
+    }
+}
