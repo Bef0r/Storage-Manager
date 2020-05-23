9a98b0ac9a74be92eda8ab9513c19c1947e65585
Albrecht Márk <alb.mark94@gmail.com>
2020-05-23 13:13:58 +0200

    Created new end point. Get role's permissions by role id.

--- src/main/java/com/storage/controllers/RoleAndPermissionController.java
+++ src/main/java/com/storage/controllers/RoleAndPermissionController.java
@@ -6,9 +6,11 @@
 package com.storage.controllers;
 
 import com.storage.api.response.RoleAndPermission.RolesAndpermissionsWithIdsResponse;
+import com.storage.api.response.RoleAndPermission.RolesPermissionsResponse;
 import com.storage.managers.interfaces.RolePermissionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,5 +25,10 @@ public class RoleAndPermissionController {
     @GetMapping(path ="/")
     public RolesAndpermissionsWithIdsResponse getRolesAndpermissionsWithIdsResponse(){
         return rolePermissionManager.getAllRolesAndPermissionsWithIds();
+    }
+    
+    @GetMapping(path ="/{roleId}")
+    public RolesPermissionsResponse getRolepermissions(@PathVariable("roleId") long roleId){
+        return rolePermissionManager.getRolePermissions(roleId);
     }
 }
