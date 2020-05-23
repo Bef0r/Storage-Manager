36c96ffc1f2e315fc62283fd4def353ee25477bd
Albrecht Márk <alb.mark94@gmail.com>
2020-05-23 13:23:31 +0200

    Added new method and formated.

--- src/main/java/com/storage/managers/mappers/RolesAndPermissionsMapper.java
+++ src/main/java/com/storage/managers/mappers/RolesAndPermissionsMapper.java
@@ -10,39 +10,41 @@ import com.storage.api.response.RoleAndPermission.RolesPermissionsResponse;
 import com.storage.repositories.entities.Permission;
 import com.storage.repositories.entities.Role;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
 public class RolesAndPermissionsMapper {
-    
-    public static Map<Long,String> mappedRolesWithId(List<Role> roles){
-        
-        Map<Long,String> RolesWithIds = new HashMap<>();
-        roles.forEach(role->{
+
+    public static Map<Long, String> mappedRolesWithId(List<Role> roles) {
+
+        Map<Long, String> RolesWithIds = new HashMap<>();
+        roles.forEach(role -> {
             RolesWithIds.put(role.getId(), role.getRoleName());
         });
-        
         return RolesWithIds;
     }
-    
-     public static  Map<Long,String> mappedPermissionsWithId(List<Permission> permisssions){
-        
-        Map<Long,String> PermissionsWithIds = new HashMap<>();
-        permisssions.forEach(permission->{
-            PermissionsWithIds.put(permission.getId(),permission.getPermissionName());
+
+    public static Map<Long, String> mappedPermissionsWithId(List<Permission> permisssions) {
+
+        Map<Long, String> PermissionsWithIds = new HashMap<>();
+        permisssions.forEach(permission -> {
+            PermissionsWithIds.put(permission.getId(), permission.getPermissionName());
         });
-        
         return PermissionsWithIds;
     }
-     
-     public static RolesAndpermissionsWithIdsResponse mappedRolesAndPermissionToResponse(Map<Long,String> roles,Map<Long,String> permissions){
-         RolesAndpermissionsWithIdsResponse response = new RolesAndpermissionsWithIdsResponse();
-         response.setRoles(roles);
-         response.setPermissions(permissions);
-         
-         return response;
-     }
+
+    public static RolesAndpermissionsWithIdsResponse mappedRolesAndPermissionToResponse(Map<Long, String> roles, Map<Long, String> permissions) {
+        RolesAndpermissionsWithIdsResponse response = new RolesAndpermissionsWithIdsResponse();
+        response.setRoles(roles);
+        response.setPermissions(permissions);
+        return response;
+    }
+
+    public static RolesPermissionsResponse getRolePermissionIdResponse(long roleId, Map<Long, String> permissions) {
+        RolesPermissionsResponse response = new RolesPermissionsResponse();
+        response.setRoleId(roleId);
+        response.setRolePermissionIds(permissions.keySet().stream().collect(Collectors.toList()));
+        return response;
+    }
 }
