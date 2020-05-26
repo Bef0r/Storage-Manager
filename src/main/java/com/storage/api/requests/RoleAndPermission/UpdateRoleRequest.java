d5cd731a3e81e6d0ee1b226d0bb69f631ee32ae0
Albrecht Márk <alb.mark94@gmail.com>
2020-05-26 13:30:09 +0200

    Created update request

---
+++ src/main/java/com/storage/api/requests/RoleAndPermission/UpdateRoleRequest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.storage.api.requests.RoleAndPermission;
+
+import java.util.List;
+
+/**
+ *
+ * @author Dell
+ */
+public class UpdateRoleRequest {
+    List<Long> permissionIds;
+
+    public List<Long> getPermissionIds() {
+        return permissionIds;
+    }
+
+    public void setPermissionIds(List<Long> permissionIds) {
+        this.permissionIds = permissionIds;
+    }
+    
+    
+}
