c06fc950a1f2fc548b3b2cc684ca594624838360
Albrecht Márk <alb.mark94@gmail.com>
2020-05-26 13:47:18 +0200

    Formated and created update method's map functions

--- src/main/java/com/storage/managers/mappers/RolesAndPermissionsMapper.java
+++ src/main/java/com/storage/managers/mappers/RolesAndPermissionsMapper.java
@@ -9,6 +9,7 @@ import com.storage.api.requests.RoleAndPermission.NewRoleRequest;
 import com.storage.api.response.RoleAndPermission.NewRoleResponse;
 import com.storage.api.response.RoleAndPermission.RolesAndpermissionsWithIdsResponse;
 import com.storage.api.response.RoleAndPermission.RolesPermissionsResponse;
+import com.storage.api.response.RoleAndPermission.UpdateRoleResponse;
 import com.storage.repositories.entities.Permission;
 import com.storage.repositories.entities.Role;
 import com.storage.repositories.entities.RolePermission;
@@ -51,16 +52,16 @@ public class RolesAndPermissionsMapper {
         response.setRolePermissionIds(permissions.keySet().stream().collect(Collectors.toList()));
         return response;
     }
-    
-    public static Role extractNewRoleNameIntheRequest(NewRoleRequest newRoleRequest){
+
+    public static Role extractNewRoleNameIntheRequest(NewRoleRequest newRoleRequest) {
         Role requestRole = new Role();
         requestRole.setRoleName(newRoleRequest.getRoleName().toUpperCase());
         return requestRole;
     }
-    
-    public static  List<RolePermission> extractRolePermissionsIntheRequest(Role newRole, List<Long> newRolePermissionIds){
-        List<RolePermission> newRolePermissions = new LinkedList<>(); 
-        newRolePermissionIds.stream().forEach(permissionId->{
+
+    public static List<RolePermission> extractRolePermissionsIntheRequest(Role newRole, List<Long> newRolePermissionIds) {
+        List<RolePermission> newRolePermissions = new LinkedList<>();
+        newRolePermissionIds.stream().forEach(permissionId -> {
             RolePermission newRolePermission = new RolePermission();
             newRolePermission.setRoleId(newRole.getId());
             newRolePermission.setPermissionId(permissionId);
@@ -68,20 +69,52 @@ public class RolesAndPermissionsMapper {
         });
         return newRolePermissions;
     }
-    
-    public static NewRoleResponse newRoleResponse(Role newRole,  List<RolePermission> newRolePermissions){
+
+    public static NewRoleResponse newRoleResponse(Role newRole, List<RolePermission> newRolePermissions) {
         NewRoleResponse response = new NewRoleResponse();
         response.setNewRoleId(newRole.getId());
         response.setNewRoleName(newRole.getRoleName());
         response.setNewRolePermissionIds(extractNewRolePermissionIds(newRolePermissions));
         return response;
     }
-    
-    private static List<Long> extractNewRolePermissionIds(List<RolePermission> newRolePermissions){
+
+    private static List<Long> extractNewRolePermissionIds(List<RolePermission> newRolePermissions) {
         List<Long> permissionIds = new LinkedList<>();
-        newRolePermissions.stream().forEach(relationship->{
+        newRolePermissions.stream().forEach(relationship -> {
             permissionIds.add(relationship.getPermissionId());
         });
         return permissionIds;
+    }
+
+    public static List<RolePermission> createRolePermissionsMapper(Long roleId, List<Long> permissionIds) {
+        List<RolePermission> relation = new LinkedList<>();
+        permissionIds.forEach(permissionId -> {
+            RolePermission newRelationship = new RolePermission();
+            newRelationship.setRoleId(roleId);
+            newRelationship.setPermissionId(permissionId);
+            relation.add(newRelationship);
+        });
+        return relation;
+    }
+
+    public static UpdateRoleResponse updateRoleResponse(List<Long> savePermissions, Long roleId, List<Long> sameIds, int deletedRows) {
+        UpdateRoleResponse response = new UpdateRoleResponse();
+        List<Long> permissionsIds = new LinkedList<>();
+        permissionsIds.addAll(savePermissions);
+        permissionsIds.addAll(sameIds);
+        response.setRoleId(roleId);
+        response.setRolePermissionRelationships(permissionsIds);
+        response.setNumberOfDeletedRows(deletedRows);
+        response.setNumberOfNewRows(savePermissions.size());
+        return response;
+    }
+    
+    public static List<RolePermission> selectionDeleteEntities(List<RolePermission> allRolePermissions, List<Long> deletePermissionIds){
+        List<RolePermission> rolePermissionsermissions = new LinkedList<>();
+        allRolePermissions.forEach(permission -> {
+            if(deletePermissionIds.contains(permission.getPermissionId()))
+                rolePermissionsermissions.add(permission);
+        });
+        return rolePermissionsermissions;
     }
 }
