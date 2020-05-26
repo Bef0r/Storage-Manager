5e980e40f30d8d895548bf87668cb995b1608c8f
Albrecht Márk <alb.mark94@gmail.com>
2020-05-26 13:38:50 +0200

    Created method. Get permission ids to Rolepermissions

--- src/main/java/com/storage/managers/customClasses/CollectionToMap.java
+++ src/main/java/com/storage/managers/customClasses/CollectionToMap.java
@@ -6,17 +6,27 @@
 package com.storage.managers.customClasses;
 
 import com.storage.repositories.entities.Permission;
-import java.util.Collection;
+import com.storage.repositories.entities.RolePermission;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 
 public class CollectionToMap {
-    public static Map<Long,String> convertCollectionToMap(Collection<Permission> permissions){
+    public static Map<Long,String> convertCollectionToMap(List<Permission> permissions){
         Map<Long,String> map = new HashMap<>();
         permissions.forEach(permission->{
             map.put(permission.getId(), permission.getPermissionName());
         });
         return map;
+    }
+    
+    public static List<Long> convertRolePermissionPermissionIdsToList(List<RolePermission> rolePermissions){
+        List<Long> permissionId = new LinkedList<>();
+        rolePermissions.forEach(relationships->{
+            permissionId.add(relationships.getPermissionId());
+        });
+        return permissionId;
     }
 }
