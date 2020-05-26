f3f18cd0e895cd31e8bb6f188930e1eb16bf4f25
Albrecht Márk <alb.mark94@gmail.com>
2020-05-26 13:32:47 +0200

    Created update end point

--- src/main/java/com/storage/controllers/RoleAndPermissionController.java
+++ src/main/java/com/storage/controllers/RoleAndPermissionController.java
@@ -6,14 +6,17 @@
 package com.storage.controllers;
 
 import com.storage.api.requests.RoleAndPermission.NewRoleRequest;
+import com.storage.api.requests.RoleAndPermission.UpdateRoleRequest;
 import com.storage.api.response.RoleAndPermission.NewRoleResponse;
 import com.storage.api.response.RoleAndPermission.RolesAndpermissionsWithIdsResponse;
 import com.storage.api.response.RoleAndPermission.RolesPermissionsResponse;
+import com.storage.api.response.RoleAndPermission.UpdateRoleResponse;
 import com.storage.managers.interfaces.RolePermissionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,4 +43,10 @@ public class RoleAndPermissionController {
     public NewRoleResponse createdNewRole(@RequestBody NewRoleRequest newRoleRequest){
         return rolePermissionManager.createNewRoleWithPermissions(newRoleRequest);
     }
+    
+    @PutMapping(path ="/{roleId}")
+    public UpdateRoleResponse updateRole(@PathVariable("roleId") long roleId, @RequestBody UpdateRoleRequest updateRoleRequest){
+        return rolePermissionManager.updateRolePermissions(roleId,updateRoleRequest);
+    }
+            
 }
