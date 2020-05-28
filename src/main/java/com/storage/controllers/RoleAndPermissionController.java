6d4e9ed85e91114e4321eef7bfb69c58ac2d8780
Albrecht Márk <alb.mark94@gmail.com>
2020-05-28 15:07:59 +0200

    Return type changed to respone entity and added language to header

--- src/main/java/com/storage/controllers/RoleAndPermissionController.java
+++ src/main/java/com/storage/controllers/RoleAndPermissionController.java
@@ -7,53 +7,74 @@ package com.storage.controllers;
 
 import com.storage.api.requests.RoleAndPermission.NewRoleRequest;
 import com.storage.api.requests.RoleAndPermission.UpdateRoleRequest;
-import com.storage.api.response.RoleAndPermission.DeleteRoleResponse;
-import com.storage.api.response.RoleAndPermission.NewRoleResponse;
-import com.storage.api.response.RoleAndPermission.RolesAndpermissionsWithIdsResponse;
-import com.storage.api.response.RoleAndPermission.RolesPermissionsResponse;
-import com.storage.api.response.RoleAndPermission.UpdateRoleResponse;
+import com.storage.controllers.exceptions.LanguageAndDescriptionSelector;
+import com.storage.controllers.exceptions.exceptionConstans.ExceptionConstans;
+import com.storage.managers.exceptions.RoleNameAlreadyExistsException;
 import com.storage.managers.interfaces.RolePermissionManager;
+import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/rest/roleandpermission")
 public class RoleAndPermissionController {
-    
+
     @Autowired
     RolePermissionManager rolePermissionManager;
 
-    
     @GetMapping()
-    public RolesAndpermissionsWithIdsResponse getRolesAndpermissionsWithIdsResponse(){
-        return rolePermissionManager.getAllRolesAndPermissionsWithIds();
+    public ResponseEntity<?> getRolesAndpermissionsWithIdsResponse() {
+        return ResponseEntity.ok(rolePermissionManager.getAllRolesAndPermissionsWithIds());
     }
-    
-    @GetMapping(path ="/{roleId}")
-    public RolesPermissionsResponse getRolepermissions(@PathVariable("roleId") long roleId){
-        return rolePermissionManager.getRolePermissions(roleId);
+
+    @GetMapping(path = "/{roleId}")
+    public ResponseEntity<?> getRolepermissions(@RequestHeader("accept-language") String language, @PathVariable("roleId") long roleId) {
+        try {
+            return ResponseEntity.ok(rolePermissionManager.getRolePermissions(roleId));
+        } catch (EntityNotFoundException entityException) {
+            return ResponseEntity.status(ExceptionConstans.ROLE_DOES_NOT_EXISTS)
+                    .body(LanguageAndDescriptionSelector.languageAndDescriptionSelector(language, ExceptionConstans.ROLE_DOES_NOT_EXISTS));
+        }
     }
-    
+
     @PostMapping()
-    public NewRoleResponse createdNewRole(@RequestBody NewRoleRequest newRoleRequest){
-        return rolePermissionManager.createNewRoleWithPermissions(newRoleRequest);
+    public ResponseEntity<?> createdNewRole(@RequestHeader("accept-language") String language, @RequestBody NewRoleRequest newRoleRequest) {
+        try {
+            return ResponseEntity.ok(rolePermissionManager.createNewRoleWithPermissions(newRoleRequest));
+        } catch (RoleNameAlreadyExistsException roleNameAlreadyExistsException) {
+            return ResponseEntity.status(ExceptionConstans.ROLE_NAME_EXISTS)
+                    .body(LanguageAndDescriptionSelector.languageAndDescriptionSelector(language, ExceptionConstans.ROLE_NAME_EXISTS));
+        }
     }
-    
-    @PutMapping(path ="/{roleId}")
-    public UpdateRoleResponse updateRole(@PathVariable("roleId") long roleId, @RequestBody UpdateRoleRequest updateRoleRequest){
-        return rolePermissionManager.updateRolePermissions(roleId,updateRoleRequest);
+
+    @PutMapping(path = "/{roleId}")
+    public ResponseEntity<?> updateRole(@RequestHeader("accept-language") String language, @PathVariable("roleId") long roleId, @RequestBody UpdateRoleRequest updateRoleRequest) {
+        try {
+            return ResponseEntity.ok(rolePermissionManager.updateRolePermissions(roleId, updateRoleRequest));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(LanguageAndDescriptionSelector.languageAndDescriptionSelector(language, ExceptionConstans.INTERNAL_SERVER_ERROR_ROLE_UPDATE));
+        }
     }
-    
-    @DeleteMapping(path ="/{roleId}")
-    public DeleteRoleResponse  deleteRoleWithPermissions(@PathVariable("roleId") long roleId){
-        return rolePermissionManager.deleteRoleWithPermissions(roleId);
+
+    @DeleteMapping(path = "/{roleId}")
+    public ResponseEntity<?> deleteRoleWithPermissions(@RequestHeader("accept-language") String language, @PathVariable("roleId") long roleId) {
+        try {
+            return ResponseEntity.ok(rolePermissionManager.deleteRoleWithPermissions(roleId));
+        } catch (EntityNotFoundException entityNotFoundException) {
+            return ResponseEntity.status(ExceptionConstans.ROLE_DOES_NOT_EXISTS)
+                    .body(LanguageAndDescriptionSelector.languageAndDescriptionSelector(language,ExceptionConstans.ROLE_DOES_NOT_EXISTS));
+        }
     }
-            
+
 }
