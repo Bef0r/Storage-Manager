4a58f2ce148406cc8955dbb60395da7a9a0c5ad7
Albrecht Márk <alb.mark94@gmail.com>
2020-05-28 15:10:23 +0200

    Added throw exception to abstract method

--- src/main/java/com/storage/managers/interfaces/RolePermissionManager.java
+++ src/main/java/com/storage/managers/interfaces/RolePermissionManager.java
@@ -12,12 +12,13 @@ import com.storage.api.response.RoleAndPermission.NewRoleResponse;
 import com.storage.api.response.RoleAndPermission.RolesAndpermissionsWithIdsResponse;
 import com.storage.api.response.RoleAndPermission.RolesPermissionsResponse;
 import com.storage.api.response.RoleAndPermission.UpdateRoleResponse;
+import com.storage.managers.exceptions.RoleNameAlreadyExistsException;
 
 
 public interface RolePermissionManager {
     public RolesAndpermissionsWithIdsResponse getAllRolesAndPermissionsWithIds();
     public RolesPermissionsResponse getRolePermissions(long roleId);
-    public NewRoleResponse createNewRoleWithPermissions(NewRoleRequest newRoleRequest);
+    public NewRoleResponse createNewRoleWithPermissions(NewRoleRequest newRoleRequest) throws RoleNameAlreadyExistsException;
     public UpdateRoleResponse updateRolePermissions(long roleId, UpdateRoleRequest updateRoleRequest);
     public DeleteRoleResponse deleteRoleWithPermissions(long roleId);
 }
