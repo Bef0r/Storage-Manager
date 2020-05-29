522efd64f3044282d578bb8b75c3b4ea3d61ab10
Albrecht Márk <alb.mark94@gmail.com>
2020-05-29 13:23:54 +0200

    Added new error code and defualt message

--- src/main/java/com/storage/controllers/exceptions/exceptionConstans/ExceptionConstans.java
+++ src/main/java/com/storage/controllers/exceptions/exceptionConstans/ExceptionConstans.java
@@ -12,7 +12,10 @@ package com.storage.controllers.exceptions.exceptionConstans;
 public interface ExceptionConstans {
     public static final int EXCEPTION_FILE_NOT_FOUND =800;
     public static final String EXCEPTION_FILE_NOT_FOUND_DESC= "Exception file not found";
+    public static final String DEFAULT_EXCEPTON_MESSAGE ="Something went wrong, try again later";
     public static final int ROLE_DOES_NOT_EXISTS = 1000;
     public static final int ROLE_NAME_EXISTS = 1001;
     public static final int INTERNAL_SERVER_ERROR_ROLE_UPDATE = 1002;
+    public static final int ROLE_UPDATE_CREATE_ERROR = 1003;
+    public static final int ROLE_UPDATE_DELETE_ERROR = 1004;
 }
