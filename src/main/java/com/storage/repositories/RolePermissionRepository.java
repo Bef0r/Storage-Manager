736420083da83374180ee18b5318123d6320d433
Albrecht Márk <alb.mark94@gmail.com>
2020-05-30 16:53:01 +0200

    Added prototype costum query

--- src/main/java/com/storage/repositories/RolePermissionRepository.java
+++ src/main/java/com/storage/repositories/RolePermissionRepository.java
@@ -7,8 +7,16 @@ package com.storage.repositories;
 
 import com.storage.repositories.entities.RolePermission;
 import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RolePermissionRepository extends CrudRepository<RolePermission, Long> {
+
     public List<RolePermission> findAllByRoleId(Long roleId);
+
+    @Modifying
+    @Query(value = "delete from RolePermission c where c.roleId = ?1")
+    public int deleteByRoleId(long roleId);
 }
