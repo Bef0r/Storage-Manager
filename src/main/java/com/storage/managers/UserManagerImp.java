b8532aa91b180ae406f05fb3708f2f7b549e0a8b
Albrecht Márk <alb.mark94@gmail.com>
2020-05-30 16:53:47 +0200

    Created user repository and save all method

--- src/main/java/com/storage/managers/UserManagerImp.java
+++ src/main/java/com/storage/managers/UserManagerImp.java
@@ -6,9 +6,26 @@
 package com.storage.managers;
 
 import com.storage.managers.interfaces.UserManager;
+import com.storage.repositories.UserRepository;
+import com.storage.repositories.entities.User;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserManagerImp implements UserManager{
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserManagerImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    
+    @Override
+    public List<User> saveAllUsers(List<User> users) {
+        return (List<User>) userRepository.saveAll(users);
+    }
     
 }
